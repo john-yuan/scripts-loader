6331328789304a46d009c596a6fa140e6ae3645e
John Yuan <jyuanco@outlook.com>
2018-12-29 14:17:08 +0800

    remove redundant line

--- scripts-loader.js
+++ scripts-loader.js
@@ -157,7 +157,6 @@ var ScriptsLoader = (function () {
                 var callback = onfinish;
                 onfinish = null;
                 callback && callback();
-                callback = null;
             }
         };
         for ( ; i < total; i += 1) {
